0107be7d410b6245ec56c5f201329248be8e981d
Itamar Alves Ferreira Junior <cdajuniorf@gmail.com>
2025-09-04 21:17:00 -0300

    feat: resources PostResource, UserResource e PostCommentResource com formato RESTful

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id'         => $this->id,
+            'name'       => $this->name,
+            'email'      => $this->email,
+            'birthdate'  => $this->birthdate?->format('Y-m-d'),
+            'city'       => $this->city,
+            'work'       => $this->work,
+            'bio'        => $this->bio,
+            'avatar_url' => $this->avatar ? asset('storage/' . $this->avatar) : null,
+            'cover_url'  => $this->cover ? asset('storage/' . $this->cover) : null,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
