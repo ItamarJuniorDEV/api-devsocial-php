d757ae9cba08063ace5a46f9c633ee5a01a2f75d
Itamar Junior <cdajuniorf@gmail.com>
2026-05-26 19:05:00 -0300

    chore: remover frontend e consolidar projeto no backend

--- frontend/src/components/AppLoading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="full-height row items-center justify-center bg-dark-page">
-    <q-spinner color="primary" size="48px" />
-  </div>
-</template>
-
-<script setup>
-//
-</script>
